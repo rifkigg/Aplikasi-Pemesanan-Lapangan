dc90a905675edeff7166d0d773a4c060d1cb1715
mhsdaniel <sulthonak13@gmail.com>
2024-01-27 14:52:27 +0700

    memperbaiki navbar lagi dan lagi

--- index.php
+++ index.php
@@ -13,11 +13,11 @@ $database = mysqli_connect("localhost", "root", "", "db_pemesanan_lapangan");
     </style>
   </head>
   <body>
-    <nav>
-      <a href="index.php">Home</a>
-      <a href="pemesanan.php">Pemesanan</a>
-      <a href="about.php">About</a>
-    </nav>
+  <nav>
+    <a href="index.php">Home</a>
+    <a href="pemesanan.php">Pemesanan</a>
+    <a href="about.php">About</a>
+</nav>
     <h1>WELCOME TO GOR SAKA SPORTS</h1>
     
   </body>
