13f74898a800d70c9820f2ac0fa3b82b9c2e89da
mhsdaniel <sulthonak13@gmail.com>
2024-01-27 14:32:05 +0700

    memperbaiki navbar

---
+++ pemesanan.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav>
+    <a href="index.php">Home</a>
+    <a href="pemesanan.php">Pemesanan</a>
+    <a href="about.php">About</a>
+</nav>
+<img src="galeri/foto 2.png" alt="">
+<h3>Harga /jam </h3>
+</body>
+</html>
