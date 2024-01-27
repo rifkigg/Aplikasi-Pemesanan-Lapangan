95cc2fe0ae579f75424f1d020e1a4f0296fbb874
mhsdaniel <sulthonak13@gmail.com>
2024-01-27 13:45:41 +0700

    Danil membuat navbar

--- index.php
+++ index.php
@@ -4,15 +4,18 @@ $database = mysqli_connect("localhost", "root", "", "db_pemesanan_lapangan");
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SAKA Sports</title>
-</head>
-<body>
-    <h1>Table Pemesanan Lapangan</h1>
-    <table>
-                                                                     
-    </table>
-</body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Aplikasi Pemesanan Lapangan</title>
+  </head>
+  <body>
+    <nav>
+      <a href="home">Home</a>
+      <a href="pemesanan">Pemesanan</a>
+      <a href="about">About</a>
+    </nav>
+    <h1>WELCOME TO GOR SAKA SPORTS</h1>
+  </body>
+</html>
 </html>
